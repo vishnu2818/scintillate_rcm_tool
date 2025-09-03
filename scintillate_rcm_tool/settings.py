56266dcd005775b63821c1ef4a273020834debe6
Vishnu <vishnuu3043@gmail.com>
2025-09-03 19:34:58 +0530

    update settings.py

--- scintillate_rcm_tool/settings.py
+++ scintillate_rcm_tool/settings.py
@@ -128,8 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
